8af8d7ee53aea3c4e6ae0e1962283542340b7f02
Simon Oulevay <simon.oulevay@lotaris.com>
2014-05-19 23:04:05 +0200

    Validate uniqueness of names.

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,4 +1,5 @@
 class Person < ActiveRecord::Base
   validates :first_name, presence: true, length: { minimum: 1, maximum: 50, allow_blank: true }
   validates :last_name, presence: true, length: { minimum: 1, maximum: 50, allow_blank: true }
+  validates :last_name, uniqueness: { scope: :first_name, message: 'and first name have already been taken' }
 end
